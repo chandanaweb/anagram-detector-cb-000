f07990353ddbae46b50a50f544c041ad2068e881
chandana M <chandu11288@gmail.com>
2018-04-18 00:46:48 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  def initialize(anagram)
+    @anagram = anagram
+  end
+  def match(str)
+    #new_arr = str.split(" ")
+    str.each do|arr|
+       if arr.split("").sort == @anagram.split("").sort
+        arr.split("")
+      else
+        nil
+      end
+    end
+  end
+end
